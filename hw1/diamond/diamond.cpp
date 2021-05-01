626732120eaf795082c262e976658497203ed409
owenmkelly <77506614+owenmkelly@users.noreply.github.com>
2021-04-30 19:21:03 -0800

    add diamond.cpp to hw1

---
+++ hw1/diamond/diamond.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main (int argc, char** argv) {
+  std::cout << "Input a positive integer." << std::endl;
+
+  int posInt = 0;
+  std::cin >> posInt;
+
+  if (posInt < 1) {
+    std::cout << "Invalid input. Please enter a positive integer" << std::endl;
+    return 1;
+  }
+
+  // Prints the top of the diamond
+  for (int line = 1; line <= posInt; line++) {
+    int numSpaces = posInt - line;
+    for (int space = 0; space < numSpaces; space++ ) {
+      std::cout << ' ';
+    }
+    int numPounds = line * 2 - 1;
+    for (int pound = 0; pound < numPounds; pound++) {
+      std::cout << '#';
+    }
+    std::cout << std::endl;
+  }
+
+  // Bottom half of the diamond
+  for (int line = 1; line < posInt; line++) {
+    for (int space = 0; space < line; space ++) {
+      std::cout << ' ';
+    }
+    int numPounds = (posInt - line) * 2 - 1;
+    for (int pound = 0; pound < numPounds; pound++) {
+      std::cout << '#';
+    }
+    std::cout << std::endl;
+  }
+
+  return 0;
+}
